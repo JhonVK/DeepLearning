7f66dbb93c1a55e770b1ad591a5e4d431b409cc9
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-06-07 22:12:50 -0300

    update

--- CriaçãodeumaAPIcomFlaskeTensorFlow2.0/app.py
+++ CriaçãodeumaAPIcomFlaskeTensorFlow2.0/app.py
@@ -20,3 +20,20 @@ model=tf.keras.models.model_from_json(model_json)
 
 #carregando pesos
 model.load_weights("fashion_model.weights.h5")
+
+#criação da API com Flask
+app=Flask(__name__)
+@app.route("/<string:img_name>", methods=["POST"])
+def classify_image(img_name):
+    upload_dir="uploads/"
+    image = imageio.v2.imread(upload_dir + img_name)
+
+
+    classes=["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+    #[1,28,28]->[1, 784]
+  
+    prediction = model.predict([image.reshape(1, 28*28)])
+    np_prediction = np.array(prediction)
+    return jsonify({"object_indentifies": classes[np.argmax(np_prediction[0])]})
+
+app.run(debug=False)
