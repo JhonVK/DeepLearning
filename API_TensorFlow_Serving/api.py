1f262fc7782378885e1121d1de86b03f5ca717cf
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-06-08 21:56:05 -0300

    update

---
+++ API_TensorFlow_Serving/api.py
@@ -0,0 +1,68 @@
+#Tf serving tem otima escalabilidade, baixa latencia, pode gerar varios modelos
+import os
+import json
+import random
+import requests
+import subprocess
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+from tensorflow import keras
+from keras.datasets import cifar10
+tf.__version__
+
+#carregando a base de dados
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+#normalizando
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+#definição do modelo
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu", input_shape=[32, 32, 3]))
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+
+#compilando
+model.compile(optimizer='Adam', 
+              loss='sparse_categorical_crossentropy', 
+              metrics=['sparse_categorical_accuracy'])
+
+#treinando
+model.fit(X_train, 
+          y_train, 
+          batch_size=128, 
+          epochs=10)
+
+#avaliando
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Test accuracy is {}".format(test_accuracy))
+
+#criando diretorio para o modelo
+model_dir = "model/"
+version = 1
+
+export_path = os.path.join(model_dir, str(version))
+
+import shutil
+if os.path.isdir(export_path):
+    shutil.rmtree(export_path)
+
+o = {t.name: t for t in model.outputs}
+
+#salvando o modelo para o tensor flow
+
+tf.saved_model.save(
+    model,
+    export_path
+)
