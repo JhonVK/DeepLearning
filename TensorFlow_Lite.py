116b988a2c5c81bb62deb29c91dff3dd2bc4aa0c
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-06-10 19:00:38 -0300

    update

--- TensorFlow_Lite.py
+++ TensorFlow_Lite.py
@@ -32,5 +32,5 @@ converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 #Salvando a versão TFLite
-with open("tflite_model", "wb") as f:
+with open("My_trained_models/tflite_model", "wb") as f:
   f.write(tflite_model)
